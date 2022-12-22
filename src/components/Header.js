345559fdfe8276c35dc7d829d2f61044ef74d784
SaurabhSahu01 <saurabhsahu794@gmail.com>
2022-12-23 03:58:00 +0530

    header, links remained

---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+function Header() {
+  return (
+    <>
+      <section className=' bg-neutral-100 flex justify-between w-11/12 mx-auto py-3 sticky top-0 border-black border-b-2'>
+        <div className="h-20 w-fit flex justify-between">
+          <img className="h-auto mr-3" src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Jawaharlal_Nehru_University_Logo_vectorized.svg/848px-Jawaharlal_Nehru_University_Logo_vectorized.svg.png?20200107230203" alt="JNU_logo" />
+          <div className='flex-col justify-between my-auto'>
+            <span className='text-3xl font-bold'>Alumni Association of JNU</span>
+            <h4 className='font-semibold'>Jawaharlal Nehru University</h4>
+          </div>
+        </div>
+        <div className='flex justify-between my-auto'>
+          <button type="button" className="btn btn-outline-primary rounded-full mr-2">Log In</button>
+          <button type="button" className="btn btn-outline-primary rounded-full mr-2">Sign Up</button>
+        </div>
+      </section>
+      <section>
+      
+      </section>
+    </>
+  )
+}
+
+export default Header;
