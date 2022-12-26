528ce6a3d91de81312dec524da492cd32e633555
SaurabhSahu01 <saurabhsahu794@gmail.com>
2022-12-26 22:38:51 +0530

    Login + signup + navbar redesigned

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,8 @@ import "../style/Navbar.css";
 import styled from 'styled-components';
 function Header() {
   return (
-    <div className='flex justify-center self-center w-11/12 mx-auto border-black border-b-2'>
-      <section className='bg-neutral-100 flex-1 justify-center '>
+    <div className='bg-neutral-100 flex justify-center self-center w-11/12 mx-auto border-black border-b-2 py-2 sticky top-0 z-50'>
+      <section className='flex-1 justify-center '>
         <div className="h-20 w-fit flex justify-between">
           <img className="h-auto mr-3" src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Jawaharlal_Nehru_University_Logo_vectorized.svg/848px-Jawaharlal_Nehru_University_Logo_vectorized.svg.png?20200107230203" alt="JNU_logo" />
           <div className='flex-col justify-between my-auto'>
@@ -16,20 +16,23 @@ function Header() {
         </div>
       </section>
       <section className='flex-2 justify-center self-center'>
-      <ul className='flex justify-center self-center gap-5' id="navbar">
-                <li className="transition duration-200 hover:scale-125 ">
-                    <StyledNavLink exact className="text-3xl font-semibold" to="/">HOME</StyledNavLink>
-                </li>
-                <li className="transition duration-200 hover:scale-125">
-                    <StyledNavLink exact className="text-3xl font-semibold" to="/about">ABOUT</StyledNavLink>
-                </li>
-                <li className="transition duration-200 hover:scale-125">
-                    <StyledNavLink exact className="text-3xl font-semibold" to="/join">JOIN</StyledNavLink>
-                </li>
-                <li className="transition duration-200 hover:scale-125">
-                    <StyledNavLink exact className="text-3xl font-semibold" to="/jobs">JOBS</StyledNavLink>
-                </li>
-            </ul>
+        <ul className='flex justify-center self-center gap-5' id="navbar">
+          <li className="transition duration-200 hover:scale-125 ">
+            <StyledNavLink exact className="text-3xl font-semibold" to="/">HOME</StyledNavLink>
+          </li>
+          <li className="transition duration-200 hover:scale-125">
+            <StyledNavLink exact className="text-3xl font-semibold" to="/about">ABOUT</StyledNavLink>
+          </li>
+          <li className="transition duration-200 hover:scale-125">
+            <StyledNavLink exact className="text-3xl font-semibold" to="/join">JOIN</StyledNavLink>
+          </li>
+          <li className="transition duration-200 hover:scale-125">
+            <StyledNavLink exact className="text-3xl font-semibold" to="/jobs">JOBS</StyledNavLink>
+          </li>
+          <li>
+            <input className="btn btn-primary bg-blue-600 rounded-full" type="button" value="Log In"/>
+          </li>
+        </ul>
       </section>
     </div>
   )
