35c67c17d805b7b3997a35a0f6691f4f67606c42
SaurabhSahu01 <saurabhsahu794@gmail.com>
2022-12-27 21:33:17 +0530

    .

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Join from "./pages/Join";
 import Jobs from "./pages/Jobs";
+import Login from "./pages/Login";
 import { Routes, Route } from "react-router-dom";
 function App() {
+  const isLogin = false;
   return (
     <>
     <Routes>
       <Route path="/" exact={true} element={<Home/>}></Route>
       <Route path="/about" exact={true} element={<About/>}></Route>
       <Route path="/join" exact={true} element={<Join/>}></Route>
-      <Route path="/jobs" exact={true} element={<Jobs/>}></Route>
+      <Route path="/jobs" exact={true} element={!isLogin ? <Login/> : <Jobs/>}></Route>
     </Routes>
     </>
   );
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import CallIcon from '@mui/icons-material/Call';
+import EmailIcon from '@mui/icons-material/Email';
+import HomeIcon from '@mui/icons-material/Home';
+import {Link} from "react-router-dom";
+import { Home } from '@mui/icons-material';
 function Footer() {
     return (
         <div className='bg-blueaccent flex w-full '>
             <div className='py-6 px-10 max-w-5xl mx-auto'>
-                <div className='grid grid-cols-3 gap-6 center'>
+                <div className='grid grid-cols-3 gap-20 center'>
                     <div className='mb-5 p-6'>
-                        <h1 className='text-2xl text-white mb-2 '>USEFUL LINKS</h1>
+                        <h1 className='text-2xl text-white mb-2 font-semibold'>USEFUL LINKS</h1>
                         <ul className='text-white'>
                             <li className='m-1'>Join</li>
                             <li className='m-1'>About</li>
@@ -14,7 +19,7 @@ function Footer() {
                         </ul>
                     </div>
                     <div className='mb-5 p-6'>
-                    <h1 className='text-2xl text-white mb-2'>SERVICES</h1>
+                    <h1 className='text-2xl text-white mb-2 font-semibold'>SERVICES</h1>
                         <ul className='text-white'>
                             <li className='m-1'>About Us</li>
                             <li className='m-1'>Terms and Conditions</li>
@@ -22,13 +27,25 @@ function Footer() {
                             <li className='m-1'>Careers</li>
                         </ul>
                     </div>
-                    <div className=' p-6'>
-                    <h1 className='text-2xl text-white mb-2'>CONTACT</h1>
-                        <ul className='text-white'>
-                            <li className='mb-3'>Alumni Association of JNU (AAJ) <br></br> AAJ Office : Room No. 101 JNU Convention Centre, JNU, New Delhi-110067, India</li>
-                            <li className='mb-3'>Mr Satyendra (Assistant Manager) +917011953096 <br></br>
-                            Dr Maheep (Convenor Membership Drive) +919911963456</li>
-                            <li className='mb-3'><a href='mailto: aajjnu1@gmail.com'>aajjnu1@gmail.com</a></li>
+                    <div className='p-6'>
+                        <h1 className='text-2xl text-white mb-2 text-center font-semibold'>CONTACT</h1>
+                        <ul className='text-white'>    
+                            <li className='mb-3 flex  self-center gap-2'>
+                                <Home fontSize='large' className="flex-2"></Home>
+                                <span className='flex-3'>Alumni Association of JNU (AAJ) AAJ Office : Room No. 101 JNU Convention Centre, JNU, New Delhi-110067, India</span>
+                            </li>
+                            <li className='mb-3 flex gap-2  self-center'>
+                                <CallIcon fontSize='large' className='flex-2'></CallIcon>
+                                <span className="flex-1">Mr Satyendra (Assistant Manager) +917011953096 
+                                Dr Maheep (Convenor Membership Drive) +919911963456</span>
+                            </li>
+                            <li className='mb-3 flex gap-2  self-center'>
+                                <EmailIcon fontSize="large" className='flex-2'></EmailIcon>
+                                <Link to="#" onClick={(e) => {
+                                    window.location.href = 'mailto: aajjnu1@gmail.com';
+                                    e.preventDefault();
+                                }} className="flex-2">aajjnu1@gmail.com</Link>
+                            </li>
                         </ul>
                     </div>
                 </div>
