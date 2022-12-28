22003b233dc970e11eb268fb3a00473ad7a2402b
raman kumar <ramankrchoudhary11@gmail.com>
2022-12-28 12:51:29 +0530

    about

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState } from "react";
 import Header from "./components/Header";
+import About from "./pages/About";
 function App() {
+  const [aboutDropDown, setAboutDropDown] = useState(false);
   return (
     <div>
       <Header></Header>
+      <span onClick={() => setAboutDropDown(!aboutDropDown)}>
+        <p>About</p>
+        {aboutDropDown && (
+          <div>
+            <ul>
+              <li>
+                <a href="about">About</a>
+              </li>
+            </ul>
+            <ul>
+              <li>
+                <a href="team">Team</a>
+              </li>
+            </ul>
+          </div>
+        )}
+      </span>
     </div>
   );
 }
---
+++ src/pages/About/components/TeamCard.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const TeamCard = () => {
+  return (
+    <div className="cards">
+      <div>{/* <img/> */}</div>
+      <div>
+        <h1>Dr. Madan Yadav</h1>
+        <h2>Joint Secretary</h2>
+        <h3>Class of 2000-2008</h3>
+      </div>
+    </div>
+  );
+};
+
+export default TeamCard;
