4fc067f509c72f21d102c01c84fb4ed89e3c2e66
SaurabhSahu01 <saurabhsahu794@gmail.com>
2023-01-04 23:08:09 +0530

    firebase analytics removed

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,6 +1,5 @@
 
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import {getAuth} from "firebase/auth";
 
 const firebaseConfig = {
@@ -16,4 +15,3 @@ const firebaseConfig = {
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app)
-const analytics = getAnalytics(app);
