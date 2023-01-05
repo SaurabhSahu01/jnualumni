0da4a7dd1e4695a8f981b65ff30d4597f0e1be63
SaurabhSahu01 <saurabhsahu794@gmail.com>
2023-01-05 14:54:18 +0530

    .

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -22,6 +22,10 @@ export const UserContextProvider = ({ children }) => {
         return signInWithPopup(auth, provider);
     }
 
+    const loginwithfacebook = () => {
+        
+    }
+
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, (currentuser) => {
             setuserData(currentuser);
