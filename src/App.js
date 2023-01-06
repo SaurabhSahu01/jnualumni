ac1a39d9a2103d54642d82aa6450bb487fe8a12c
SaurabhSahu01 <saurabhsahu794@gmail.com>
2023-01-06 20:18:53 +0530

    registration forms added

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import { Routes, Route } from "react-router-dom";
 import Signup from "./pages/Signup";
 import { ProtectedRoute } from "./context/ProtectedRoute";
 import { UserContextProvider } from "./context/UserContext";
+import Registration from "./pages/Registration";
 function App() {
   return (
     <>
@@ -17,6 +18,7 @@ function App() {
         <Route path="/signup" exact={true} element={<Signup />}></Route>
         <Route path="/join" exact={true} element={<ProtectedRoute><Join /></ProtectedRoute>}></Route>
         <Route path="/jobs" exact={true} element={<ProtectedRoute><Jobs /></ProtectedRoute>}></Route> 
+        <Route path="/registration" exact={true} element={<ProtectedRoute><Registration /></ProtectedRoute>}></Route> 
         <Route path="/about" exact={true} element={<About />}></Route>
         <Route path="/login" exact={true} element={<Login />}></Route> 
       </Routes>
