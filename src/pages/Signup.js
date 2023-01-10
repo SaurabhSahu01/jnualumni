3d7038eed67b37fb74beecbb3e2e127851d34070
SaurabhSahu01 <saurabhsahu794@gmail.com>
2023-01-11 02:41:42 +0530

    sign up page

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -4,9 +4,9 @@ import {useUserAuthContext} from "../context/UserContext"
 import Loader from "../components/Loader/Loader";
 function Signup() {
   const {signupwithemail, logInProgress} = useUserAuthContext();
+  const [validEmail, setvalidemail] = useState(false)
   const navigate = useNavigate();
   const [data, setData] = useState({
-    name: "",
     email:"",
     password: "",
   });
@@ -20,6 +20,7 @@ function Signup() {
   }
   const handleEmail = (e) => {
     if(validateEmail(e.target.value)){
+      setvalidemail(true);
       setData({...data, email: e.target.value})
     }
     else{
@@ -29,35 +30,21 @@ function Signup() {
   const handlePass = (e) => {
     setData({...data, password: e.target.value});
   }
-  const handleUsername = (e)=> {
-    setData({...data, name: e.target.value});
-  }
+  
   const signupHandler =  async (e) => {
     e.preventDefault();
-    if(data.name !== "" && data.email !== "" && data.password !== ""){
-      signupwithemail(data.email, data.password);
-    }
+    signupwithemail(data.email, data.password);
   }
 
   return (
     <>
       {logInProgress ? <Loader></Loader> : <></>}
       <div className="w-full h-[86vh] grid place-items-center ">
-        <div className="w-1/3 rounded-2xl py-8 bg-[#030503] flex flex-col justify-center items-center gap-4">
-          <h1 className="text-3xl text-[#ddeddd]">Create Your Account</h1>
+        <div className="w-1/3 rounded-2xl py-8 bg-[#89c1fd8e] flex flex-col justify-center items-center gap-4">
+          <h1 className="text-3xl">Create Your Account</h1>
           <div className="flex w-4/5 flex-col gap-2">
             <div className="flex flex-col gap-1">
-              <h1 className="text-[#ddeddd] text-base">Full Name </h1>
-              <input
-                className="rounded-lg py-1 px-2 outline-none"
-                autoComplete="off"
-                type="text"
-                name="name"
-                onChange={handleUsername}
-              />
-            </div>
-            <div className="flex flex-col gap-1">
-              <h1 className="text-[#ddeddd] text-base">Email Address</h1>
+              <h1 className="text-base">Email Address</h1>
               <input
                 className=" rounded-lg py-1 px-2 outline-none"
                 type="email"
@@ -67,7 +54,7 @@ function Signup() {
               />
             </div>
             <div className="flex flex-col gap-1">
-              <h1 className="text-[#ddeddd] text-base">Password </h1>
+              <h1 className="text-base">Password </h1>
               <input
                 className=" rounded-lg py-1 px-2 outline-none"
                 type="password"
@@ -76,11 +63,14 @@ function Signup() {
                 onChange={handlePass}
               />
             </div>
-            <button className="mt-4 text-[#ddeddd] rounded-lg py-2 bg-[#0b781d]" onClick={signupHandler}>
+            {(validEmail && data.password !== "")? (<button className="text-white mt-4 rounded-lg py-2 bg-[#0b781d]" onClick={signupHandler}>
               Sign Up
-            </button>
+            </button>) : (<button className="text-white mt-4 rounded-lg py-2 bg-[#0b781d57] cursor-not-allowed" disabled>
+              Sign Up
+            </button>)}
+            
             <button
-              className="text-xs mt-6 text-[#ddeddd] bg-transparent hover:text-sky-700"
+              className="text-blue-800 text-md mt-6 bg-transparent"
               onClick={() => navigate("/login")}
             >
               Already have account! log in
