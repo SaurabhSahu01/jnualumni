5dce3a22e3d6d1082a93084dc63fdbf28649faae
SaurabhSahu01 <saurabhsahu794@gmail.com>
2023-01-11 11:17:11 +0530

    storage key updated

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -6,7 +6,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyA3TPjryklZyBSjArNQM1l9lj5qXWiJ7MY",
   authDomain: "jnualumni-9b709.firebaseapp.com",
   projectId: "jnualumni-9b709",
-  storageBucket: "learningfirebase-32fbd.appspot.com",
+  storageBucket: "gs://jnualumni-9b709.appspot.com",
   messagingSenderId: "1059990415542",
   appId: "1:1059990415542:web:e3abf24d3b12df921c5e17",
   measurementId: "G-9QCKVQ3HG0",
