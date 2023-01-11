23858d9c0d3abe84397d95a81c9d066190e25c50
SaurabhSahu01 <saurabhsahu794@gmail.com>
2023-01-11 10:53:18 +0530

    events and gallery

---
+++ src/pages/AddEvent.js
@@ -0,0 +1,90 @@
+import React ,{useState} from 'react'
+import {addDoc, collection} from 'firebase/firestore';
+import { auth ,db, storage} from '../firebase/firebase';
+import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
+
+
+
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
+function AddEvent() {
+
+  const [data, setdata] = useState({
+    title: "",
+    description: "",
+    imageurl:""
+  });
+  const [file, setfile] = useState(null);
+
+  const inputEvent = (e) => {
+    const { name, value } = e.target;
+    setdata((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    })
+    ;
+  };
+
+  const upload = async (e) => {
+    e.preventDefault();
+    e.preventDefault();
+    if (!data.title || !data.description) {
+      alert("invalid details");
+    }
+    try {
+      const storageRef = await ref(storage,file.name);
+      const uploadTask = await uploadBytesResumable(storageRef,file)
+      console.log(uploadTask)
+       const downloadurl = await getDownloadURL(uploadTask.ref);
+       console.log(downloadurl)
+       await setdata( (prev)=>({...prev, imageurl:downloadurl}))
+       await addDoc(collection(db, "event"), {
+        ...data,
+        imageurl:downloadurl, 
+      });
+     
+    } catch (err) {
+      
+      alert(err);
+    }
+
+
+
+  }
+
+  return (
+    <div>
+
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label></Form.Label>Title
+          <Form.Control type="text" placeholder="Title" name="title" value={data.title} onChange={inputEvent}/>
+          <Form.Text className="text-muted">
+            add title
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Description</Form.Label>
+          <Form.Control type="text" placeholder="add description" name="description" value={data.description} onChange={inputEvent}/>
+        </Form.Group>
+
+        <div>
+          <label for="formFileLg" class="form-label">upload cover images</label>
+          <input class="form-control form-control-lg"  type="file" name="imageurl" onChange={(e) =>{ setfile(()=> e.target.files[0])
+          console.log(e.target.files[0].name)}} />
+        </div>
+        <Button variant="primary" type="submit" onClick={upload} > 
+          Submit
+        </Button>
+      </Form>
+
+
+    </div>
+  )
+}
+
+export default AddEvent;
---
+++ src/pages/ShowGallary.js
@@ -0,0 +1,49 @@
+import React, {useEffect, useState} from 'react'
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Car2 from '../images/NirmalaSitaraman.webp';
+import {db} from '../firebase/firebase';
+import { collection, doc,getDocs } from "firebase/firestore";
+import "./Pages.css"
+
+function ShowGallary() {
+
+  const[imagearr, setimagearr] = useState([])
+    
+    const getImageUrl = async()=>{
+        try{
+            const docref = await collection(db,"gallary");
+            // console.log("dataurl" ,docref)
+            const data = await getDocs(docref);
+            // data.docs.map((doc) => (console.log({ ...doc.data() })))
+            setimagearr(data.docs.map((doc) => ({ ...doc.data() })))
+        if(data){
+           console.log(imagearr)
+        }
+ 
+        }catch(err){
+            alert(err)
+        }
+       
+
+    }
+
+    useEffect(() => {
+      getImageUrl();
+    }, []);
+
+   
+
+    return (
+        <div className='show__gallary'>
+        <div className='event__name'> Gallary</div>
+        {imagearr.map((image)=>{
+            return<><Card style={{ width: '18rem' }}> <Card.Img variant="top" src={image.imageurl}  key={Math.random()}/>  <Card.Body >{image.title}</Card.Body>  </Card> <br></br></>
+        })}
+           
+         
+        </div>
+    )
+}
+
+export default ShowGallary
