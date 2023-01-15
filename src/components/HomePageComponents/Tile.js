845f1f1cf4b0ac85127b66b65324832609fa18b2
SaurabhSahu01 <saurabhsahu794@gmail.com>
2023-01-15 20:47:49 +0530

    .

--- src/components/HomePageComponents/Tile.js
+++ src/components/HomePageComponents/Tile.js
@@ -3,7 +3,7 @@ function Tile(props) {
     return (
         <div className='flex flex-row'>
             <div className='p-2 w-1/6'>
-                <img src={props.src} className='w-16 h-16 rounded-sm p-1 object-cover self-center m-1'></img>
+                <img src={props.src} alt="img" className='w-16 h-16 rounded-sm p-1 object-cover self-center m-1'/>
             </div>
             <div className='w-5/6'>
                 <p className='text-l font-bold'>{props.title}</p>
