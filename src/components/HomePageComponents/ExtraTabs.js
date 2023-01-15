8b78a4a951d17e93cd816c2a0d2f1ea529365e3b
SaurabhSahu01 <saurabhsahu794@gmail.com>
2023-01-15 20:44:46 +0530

    extra tabs

--- src/components/HomePageComponents/ExtraTabs.js
+++ src/components/HomePageComponents/ExtraTabs.js
@@ -5,14 +5,22 @@ import Calendar from '../../icons/calendar.png'
 import Newspaper from '../../icons/newspaper.png'
 import Briefcase from '../../icons/briefcase.png'
 const ExtraTabs = () => {
-    return ( 
-    <div className='w-5/6 flex flex-row justify-around mx-auto  p-8 rounded-2xl mb-5 bg-gradient-to-b from-yellow-700 via-yellow-500 to-yellow-700' >
-        <Tab src={Gallery} title='Gallery'/>
-        <Tab src={Calendar} title='Events'/>
-        <Tab src={Newspaper} title='Alumni News'/>
-        <Tab src={Briefcase} title='Career Opportunity'/>    
-    </div>
-     );
+    return (
+        <div className='w-5/6 flex flex-row flex-wrap justify-around mx-auto  p-8 rounded-2xl mb-5 bg-gradient-to-b from-yellow-700 via-yellow-500 to-yellow-700' >
+            <div className='w-full sm:w-1/2 md:w-1/4'>
+                <Tab src={Gallery} title='Gallery' />
+            </div>
+            <div className='w-full sm:w-1/2 md:w-1/4'>
+                <Tab src={Calendar} title='Events' />
+            </div>
+            <div className='w-full sm:w-1/2 md:w-1/4'>
+                <Tab src={Newspaper} title='Alumni News' />
+            </div>
+            <div className='w-full sm:w-1/2 md:w-1/4'>
+                <Tab src={Briefcase} title='Career Opportunity' />
+            </div>
+        </div>
+    );
 }
- 
+
 export default ExtraTabs;
